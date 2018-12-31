28538a2e6e5c1721c7bb1acf0980fb752e2be1a8
Andres <andreschabba@gmail.com>
2018-12-31 10:14:48 -0600

    actualizacion

--- server/server.js
+++ server/server.js
@@ -9,16 +9,10 @@ const path = require('path'); //paquete que trae node por defecto, crea el path 
 //INICIAR CONEXION A BASE DE DATOS
 //"C:\Program Files\MongoDB\Server\4.0\bin\mongod.exe" --dbpath="c:\data\db"
 
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
-
-// get - obtener informacion
-// post - crear nuevos registros
-// put - actualizar registros
-// delete - cambiar el estado a no disoponible
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
